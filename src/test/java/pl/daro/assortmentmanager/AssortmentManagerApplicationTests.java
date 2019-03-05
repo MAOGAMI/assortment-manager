d1440bc134f5850ed718216e42f46a59b400d6cc
Mao <28206886+MAOGAMI@users.noreply.github.com>
2019-03-05 20:18:42 +0100

    initial commit

---
+++ src/test/java/pl/daro/assortmentmanager/AssortmentManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.daro.assortmentmanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AssortmentManagerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
